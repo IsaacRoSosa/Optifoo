ba625bbd45885894db1ec52dce8ba194c19a0a10
Isaac Rojas <isaacrojassosa@gmail.com>
2024-10-22 23:27:21 -0600

    NavBar Component

--- src/app/page.js
+++ src/app/page.js
@@ -9,11 +9,7 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <div className={styles.header}>
-        <Image src="/Images/sidebar/Jobmiga_logo.png" alt="Jobmiga Logo" width={90} height={90} />
-        <h1 className={styles.brand}>JOBHILL</h1>
-
-      </div>
+      <h1>My Pantry</h1>
 
 
 
