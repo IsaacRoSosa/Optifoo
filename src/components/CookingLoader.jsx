d70a7942b87fcca199fd6ca71ba17d8868d8d986
Isaac Rojas <isaacrojassosa@gmail.com>
2024-10-25 05:32:50 -0600

    SemiFunctional

--- src/components/CookingLoader.jsx
+++ src/components/CookingLoader.jsx
@@ -1,11 +1,11 @@
 // components/CookingLoader.js
 import React from 'react';
-import styles from './CookingLoader.module.css'; // Importamos el CSS
+import styles from '@/styles/CookingLoader.module.css'; // Importamos el CSS
 
 const CookingLoader = () => {
   return (
     <div className={styles.container}>
-      <h1>Cooking in progress</h1>
+      <h1 className={styles.title}>Cooking in progress</h1>
       <div className={styles.cooking}>
         <div className={styles.bubble}></div>
         <div className={styles.bubble}></div>
