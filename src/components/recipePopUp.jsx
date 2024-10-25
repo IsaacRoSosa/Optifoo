b5b72c5e1950d7a5022f732b786d1e8599204f46
Isaac Rojas <isaacrojassosa@gmail.com>
2024-10-25 08:27:15 -0600

    Fetch Correctly recipes from database

--- src/components/recipePopUp.jsx
+++ src/components/recipePopUp.jsx
@@ -1,6 +1,6 @@
 import styles from '@/styles/recipePopup.module.css';
 
-export default function RecipePopup({ recipe, onClose }) {
+export default function RecipePopup({ recipe, onClose, onLike, onCook }) {
   if (!recipe) return null;
 
   return (
@@ -10,36 +10,63 @@ export default function RecipePopup({ recipe, onClose }) {
           <img className={styles.close} src="/Images/icons/cloose.png" alt="close" />
         </button>
 
-        <h2 className={styles.recipeTitle}>{recipe.title}</h2>
-        <img
-          src={recipe.image || '/Images/icons/OptiChef.png'} // Imagen por 
-          alt={recipe.title}
-          className={styles.recipeImage}
-        />
+        <div className={styles.recipeImageContainer}>
+          <img
+            src={recipe.image || '/Images/icons/OptiChef.png'} 
+            alt={recipe.title}
+            className={styles.recipeImage}
+          />
+          <button
+            className={styles.heartButton}
+            onClick={(e) => {
+              e.stopPropagation(); // Evitar cierre del popup
+              onLike(recipe.title); // Manejar "like" del usuario
+            }}
+          >
+            ❤️
+          </button>
+        </div>
 
-        <div className={styles.columns}>
+        <div className={styles.recipeDetails}>
+          <h3 className={styles.recipeTitle}>{recipe.title}</h3>
 
-          <div className={styles.ingredients}>
-            <h3>Ingredients</h3>
-            <ul>
-              {recipe.ingredients.map((ingredient, index) => (
-                <li key={ingredient.idProduct || index}>
-                  <strong>{ingredient.name}</strong>: {ingredient.quantity}
-                  {ingredient.description && ` (${ingredient.description})`}
-                </li>
-              ))}
-            </ul>
+          <div className={styles.infoBlock}>
+            <p className={styles.info}>
+              <img src="/Images/icons/utensils.png" alt="category" className={styles.icon} />
+              {recipe.categories.join(', ')}
+            </p>
+            <p className={styles.info}>
+              <img src="/Images/icons/time.png" alt="time" className={styles.icon} />
+              {recipe.timeToPrepare}
+            </p>
           </div>
 
-          {/* Instrucciones */}
-          <div className={styles.steps}>
-            <h3>Instructions</h3>
-            <ol>
-              {recipe.steps.map((step, index) => (
-                <li key={index}>{step}</li>
-              ))}
-            </ol>
+          <div className={styles.columns}>
+            <div className={styles.ingredients}>
+              <h3 className={styles.subTitles}>Ingredients</h3>
+              <ul className={styles.listIngredients}>
+                {recipe.ingredients.map((ingredient, index) => (
+                  <li className={styles.text} key={ingredient.idProduct || index}>
+                    <strong>{ingredient.name}</strong>: {ingredient.quantity}
+                    {ingredient.description && ` (${ingredient.description})`}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className={styles.instructions}>
+              <h3 className={styles.subTitles}>Instructions</h3>
+              <ol>
+                {recipe.steps.map((step, index) => (
+                  <li className={styles.text} key={index}>{step}</li>
+                ))}
+              </ol>
+            </div>
           </div>
+
+          <button className={styles.cookButton} onClick={() => onCook(recipe.ingredients)}>
+            Cook
+          </button>
         </div>
       </div>
     </div>
